1a983b2a0aa3daef790bd576acfb346abec0e620
Pinkosaur <gaetan.thomassin@gmail.com>
2023-06-20 15:52:02 +0200

    update

---
+++ SAE24/partieweb/recuperation.py
@@ -0,0 +1,10 @@
+import os, json
+from .views import ajoutdonnees
+
+repertoire = './messages'
+
+while True:
+    if os.listdir(repertoire):
+        for file in os.listdir(repertoire):
+            with open(os.path.join(repertoire, file), 'r') as f:
+                data = json.load(f)
